818af8bbd4768999f8f216251eeea85759446f3d
atandiedwin <developeredwin2016@gmail.com>
2020-01-29 18:31:08 +0300

    test

--- app/src/main/java/com/atandi/smarttravel/Constants/Links.java
+++ app/src/main/java/com/atandi/smarttravel/Constants/Links.java
@@ -6,4 +6,5 @@ public class Links {
     public static  final  String FETCH_DETAILS= "http://192.168.157.160/SmartTravel/vehicles.php";
     public static  final  String SAVE_DETAILS= "http://192.168.157.160/SmartTravel/summary.php";
     public static  final  String FETCH_TRIPS= "http://192.168.157.160/SmartTravel/trips.php";
+    public  String boy = "boy";
 }
